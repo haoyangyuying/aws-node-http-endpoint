019ca0e57ae823940326ebc00ea0b66d47ecdf2a
Howard Yang <howard@shoplineapp.com>
2020-04-18 16:01:02 +0800

    feat(AWSAPI): update CI/CD setting at dashboard

--- handler.js
+++ handler.js
@@ -6,6 +6,7 @@ module.exports.endpoint = (event, context, callback) => {
     body: JSON.stringify({
       message1: `Hello, the current time is ${new Date().toTimeString()}.`,
       message2: 'this is updated by howard',
+      message3: 'updated after CI/CD setting'
     }),
   };
 
